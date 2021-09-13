041e0ce87f11f66e00aa3fb16a047cfd7a433e01
Ivan Lazarevski <Ivan.Lazarevski@seavus.com>
2021-09-13 20:31:08 +0200

    Finalized directives code

--- class_03-Lifecycle-Hooks-and-Directives/src/app/directives/hover-shadow.directive.ts
+++ class_03-Lifecycle-Hooks-and-Directives/src/app/directives/hover-shadow.directive.ts
@@ -1,10 +1,16 @@
-import { Directive, OnInit, ElementRef, HostListener } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  ElementRef,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHoverShadow]',
 })
 export class HoverShadowDirective implements OnInit {
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
     // this.elementRef.nativeElement.style.backgroundColor = 'green';
@@ -12,11 +18,17 @@ export class HoverShadowDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseOver() {
-    this.elementRef.nativeElement.style.boxShadow =
-      '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)';
+    // this.elementRef.nativeElement.style.boxShadow =
+    //   '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)';
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'box-shadow',
+      '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
+    );
   }
 
   @HostListener('mouseleave') mouseLeave() {
-    this.elementRef.nativeElement.style.boxShadow = 'none';
+    // this.elementRef.nativeElement.style.boxShadow = 'none';
+    this.renderer.setStyle(this.elementRef.nativeElement, 'box-shadow', 'none');
   }
 }
